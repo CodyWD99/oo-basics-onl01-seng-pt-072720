afda0abc368ae5e1a51ee041c25303afbc48734a
Cody Dupuis <codydupuis99@gmail.com>
2020-08-13 09:40:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
-# Make your shoe class here!
+
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  def initialize(brand)
+  end
+  
+  def phrase
+    puts "The shoe has been repaired."
+  end
+  
+  def change_con
+    @condition = "Nike"
+  end
+end
